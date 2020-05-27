adfae9be2ea291b09041cde8b771e83024f6b94a
Marc Görtz <mail@marcgoertz.de>
2020-05-27 22:18:04 +0200

    Disguise as Safari to handle browser sniffing.

--- src/parse-manifest.js
+++ src/parse-manifest.js
@@ -9,8 +9,12 @@ const hasValidColor = response => response && response.background_color;
 const isPNG = icon => icon.type === 'image/png' || icon.src.endsWith('.png');
 const isSquare = icon => {
 	const size = icon.sizes && icon.sizes.split(' ')[0];
-	const [width, height] = size.split('x');
-	return width === height;
+	if (size && size.contains('x')) {
+		const [width, height] = size.split('x');
+		return width === height;
+	}
+
+	return false;
 };
 
 const getMaxSize = sizes => Number(sizes.split(' ').sort((a, b) => Number(b.split('x')[0]) - Number(a.split('x')[0])).shift().split('x')[0]);
@@ -20,6 +24,7 @@ const getLargestSquareIcon = icons => {
 		icon.purpose !== 'maskable' && isPNG(icon) && isSquare(icon)
 	);
 
+	// This loop is faster than .reduce()
 	let max = squarePNGs[0];
 	const size = squarePNGs.length;
 	let index;
@@ -38,19 +43,24 @@ const getLargestSquareIcon = icons => {
 
 module.exports = async url => {
 	try {
-		const response = await got(url).json();
+		const response = await got(url, {
+			headers: {
+				// Diguise as Safari to handle browser sniffing
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'
+			}
+		}).json();
 
 		if (!hasValidName(response)) {
 			console.error(logSymbols.error, 'No name or short_name given.');
 			return;
 		}
 
-		if (!hasValidIcons) {
+		if (!hasValidIcons(response)) {
 			console.error(logSymbols.error, 'No icons given.');
 			return;
 		}
 
-		if (!hasValidColor) {
+		if (!hasValidColor(response)) {
 			console.error(logSymbols.error, 'No background_color given.');
 			return;
 		}
