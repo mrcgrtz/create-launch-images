eddb3ad5f8fa00334ae2723e2940d641f2f21f0b
Marc Görtz <mail@marcgoertz.de>
2020-12-27 16:35:45 +0100

    Add support for iPhone 11/11 Pro/11 Pro Max/12/12 mini/12 Pro/12 Pro Max.

--- src/devices.js
+++ src/devices.js
@@ -2,19 +2,31 @@
 
 module.exports = [
 	{
-		// iPhone Xs Max (1242px x 2688px)
+		// iPhone 12 Pro Max (1284 x 2778px)
+		width: 428,
+		height: 926,
+		dpi: 3
+	},
+	{
+		// iPhone 12, 12 Pro (1170px x 2532px)
+		width: 390,
+		height: 844,
+		dpi: 3
+	},
+	{
+		// iPhone Xs Max, 11 Pro Max (1242px x 2688px)
 		width: 414,
 		height: 896,
 		dpi: 3
 	},
 	{
-		// iPhone XR (828px x 1792px)
+		// iPhone XR, 11 (828px x 1792px)
 		width: 414,
 		height: 896,
 		dpi: 2
 	},
 	{
-		// iPhone X, Xs (1125px x 2436px)
+		// iPhone X, Xs, 11 Pro, 12 mini (1125px x 2436px)
 		width: 375,
 		height: 812,
 		dpi: 3
