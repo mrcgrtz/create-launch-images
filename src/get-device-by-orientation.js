3e07c33495e98a728d50723fc175379eb3db341b
Marc Görtz <mail@marcgoertz.de>
2021-04-25 13:19:44 +0200

    Move device orientation logic into its own file.

---
+++ src/get-device-by-orientation.js
@@ -0,0 +1,37 @@
+const devices = require('./devices');
+
+/**
+ * Rotate device size.
+ *
+ * @param {object} device        Device data
+ * @param {number} device.width  Device width
+ * @param {number} device.height Device height
+ * @param {number} device.dpi    Device DPI
+ * @return {object} Rotated device data
+ */
+const rotate = device => ({
+	...device,
+	width: device.height,
+	height: device.width
+});
+
+/**
+ * Get device by orientation.
+ *
+ * @param {"portrait" | "portrait-primary" | "portrait-secondary" | "landscape" | "landscape-primary" | "landscape-secondary" | "any"} orientation Device orientation (via manifest)
+ * @return {array} Devices
+ */
+module.exports = orientation => devices.flatMap(device => {
+	// "portrait", "portrait-primary", "portrait-secondary"
+	if (orientation.startsWith('portrait')) {
+		return device;
+	}
+
+	// "landscape", "landscape-primary", "landscape-secondary"
+	if (orientation.startsWith('landscape')) {
+		return rotate(device);
+	}
+
+	// "any"
+	return [device, rotate(device)];
+});
--- src/lib.js
+++ src/lib.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
 
 const {logError} = require('./message');
-const devices = require('./devices');
+const getDeviceByOrientation = require('./get-device-by-orientation');
 const createImage = require('./create-image');
 const parseManifest = require('./parse-manifest');
-
-const rotate = device => ({
-	...device,
-	width: device.height,
-	height: device.width
-});
 
 module.exports = (url, flags) => (async () => {
 	try {
@@ -20,33 +14,16 @@ module.exports = (url, flags) => (async () => {
 				fs.mkdirSync(flags.outputDir);
 			}
 
-			devices
-				.flatMap(device => {
-				// "portrait", "portrait-primary", "portrait-secondary"
-					if (orientation.startsWith('portrait')) {
-						return device;
-					}
-
-					// "landscape", "landscape-primary", "landscape-secondary"
-					if (orientation.startsWith('landscape')) {
-						return rotate(device);
-					}
-
-					// "any"
-					return [
-						device,
-						rotate(device)
-					];
-				})
-				.forEach(async device => {
-					createImage({
-						...device,
-						...flags,
-						name,
-						color,
-						icon
-					});
+			const devices = getDeviceByOrientation(orientation);
+			for (const device of devices) {
+				createImage({
+					...device,
+					...flags,
+					name,
+					color,
+					icon
 				});
+			}
 		}
 	} catch {
 		logError(`Could not parse ${url}`);
