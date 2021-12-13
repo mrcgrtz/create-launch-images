331415f3233ea0aeda3ea2a61bad9a538dde43af
Marc Görtz <mail@marcgoertz.de>
2021-12-13 14:11:36 +0100

    Re-enable retries and persist response in tests.

--- src/get-manifest.js
+++ src/get-manifest.js
@@ -3,7 +3,6 @@ import got from 'got';
 const getManifest = async url => {
 	try {
 		const manifest = await got(url, {
-			retry: 0,
 			headers: {
 				// Diguise as Safari to handle browser sniffing
 				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15',
--- tests/get-manifest.test.js
+++ tests/get-manifest.test.js
@@ -6,26 +6,30 @@ import fixture from './fixtures/manifest.js';
 const basePath = 'https://example.com';
 const url = '/example.webmanifest';
 
-nock(basePath).get(url).reply(200, fixture);
+const scopeOk = nock(basePath).get(url).reply(200, fixture).persist();
 test('Gets a manifest', async t => {
 	const manifest = await getManifest(`${basePath}${url}`);
 	t.deepEqual(manifest, fixture);
 });
+scopeOk.persist(false);
 
-nock(basePath).get(url).reply(200, 'I am just a text file.');
+const scopeOkNonJSON = nock(basePath).get(url).reply(200, 'I am just a text file.').persist();
 test('Handles non-JSON content', async t => {
 	const manifest = await getManifest(`${basePath}${url}`);
 	t.is(manifest, undefined);
 });
+scopeOkNonJSON.persist(false);
 
-nock(basePath).get(url).reply(404);
+const scopeError = nock(basePath).get(url).reply(404).persist();
 test('Handles non-existent manifests', async t => {
 	const manifest = await getManifest(`${basePath}${url}`);
 	t.is(manifest, undefined);
 });
+scopeError.persist(false);
 
-nock(basePath).get(url).reply(200, fixture);
+const scopeBroken = nock(basePath).get(url).reply(200, fixture).persist();
 test('Handles missing or broken URLs', async t => {
 	const manifest = await getManifest(url);
 	t.is(manifest, undefined);
 });
+scopeBroken.persist(false);
